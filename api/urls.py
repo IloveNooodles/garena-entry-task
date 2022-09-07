303931dbadda79e3152b6a6ba457fe2be19a6444
IloveNooodles <mgarebaldhie80@gmail.com>
2022-09-08 00:01:36 +0700

    feat: add enpoint on input_ref

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import heroes, register, find_user, login, edit_profile
+from .views import heroes, register, find_user, login, edit_profile, input_ref
 
 urlpatterns = [
     path("heroes", heroes, name="heroes"),
     path("register", register, name="register"),
     path("users", find_user, name="users"),
     path("login", login, name="login"),
-    path("edit_profile", edit_profile, name="edit_profile")
+    path("edit_profile", edit_profile, name="edit_profile"),
+    path("input_ref", input_ref, name="input_ref"),
 ]
